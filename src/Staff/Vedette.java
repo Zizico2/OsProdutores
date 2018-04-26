e87a65bba32268839e98fede9ee60c32cf61d7d1
Tiago "KEBAB" Guerreiro <mewseven7777777@gmail.com>
2018-04-27 00:02:07 +0100

    Test 13 in progress almost

--- src/Staff/Vedette.java
+++ src/Staff/Vedette.java
@@ -4,7 +4,11 @@ import Array.Array;
 
 public interface Vedette{
 
-    void mope(String name);
+    void mope(StaffMember staffMember);
 
     String getBlacklist();
+
+    void reconcile(String name);
+
+    Array<StaffMember> getBlacklistArray();
 }
